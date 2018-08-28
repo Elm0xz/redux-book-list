a279d3e8e29368949b65d9816e8addbd4255adac
Przemyslaw_Etz <przemyslaw.etz@gmail.com>
2018-08-28 17:42:31 +0200

    Added actions & reducers from selected book

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,9 @@
+//reducer responsible for creating values for books
 export default function() {
     return [
-        { title: 'Harry Potter' },
-        { title: 'The Witcher' },
-        { title: 'The Bible' },
-        { title: 'Monty Python' }
+        { title: 'Harry Potter', pages: 456 },
+        { title: 'The Witcher' , pages: 385},
+        { title: 'The Bible' , pages: 954},
+        { title: 'Monty Python', pages: 121 }
     ];
-}
+} 
